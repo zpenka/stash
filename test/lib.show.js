1c452307d6646e75aac38a07bf85e7d28086219a
Zack Penka <zpenka@gmail.com>
2018-08-19 16:57:48 -0600

    Can sync song performances

--- test/lib.show.js
+++ test/lib.show.js
@@ -1,4 +1,5 @@
 const expect = require('chai').expect;
+const P = require('bluebird');
 
 const sinon = require('./helper').sinon;
 const db = require('../db');
@@ -252,6 +253,199 @@ describe('lib/show', () => {
           });
 
           return db('songs').select().then((rows) => {
+            expect(rows.length).to.equal(14);
+          });
+        });
+      });
+    });
+
+    it('persists song performances to the database', () => {
+      return show.sync('1997-12-29').then(() => {
+        const sql = `
+          SELECT
+            st.identifier           AS set
+            , sp.song_number_in_set AS song_number
+            , sh.date               AS show_date
+            , ss.identifier         AS song
+          FROM song_performances sp
+          JOIN songs             ss ON ss.id = sp.song_id
+          JOIN sets              st ON st.id = sp.set_id
+          JOIN shows             sh ON sh.id = sp.show_id
+        `;
+
+        return db.raw(sql).then((results) => {
+          const rows = results.rows;
+
+          expect(rows).to.deep.equal([
+            {
+              song: 'Nicu',
+              set: '1',
+              song_number: 1,
+              show_date: '1997-12-29',
+            },
+            {
+              song: 'Golgi Apparatus',
+              set: '1',
+              song_number: 2,
+              show_date: '1997-12-29',
+            },
+            {
+              song: 'Crossroads',
+              set: '1',
+              song_number: 3,
+              show_date: '1997-12-29',
+            },
+            {
+              song: 'Cars Trucks Buses',
+              set: '1',
+              song_number: 4,
+              show_date: '1997-12-29',
+            },
+            {
+              song: 'Train Song',
+              set: '1',
+              song_number: 5,
+              show_date: '1997-12-29',
+            },
+            {
+              song: 'Theme From The Bottom',
+              set: '1',
+              song_number: 6,
+              show_date: '1997-12-29',
+            },
+            {
+              song: 'Fluffhead',
+              set: '1',
+              song_number: 7,
+              show_date: '1997-12-29',
+            },
+            {
+              song: 'Dirt',
+              set: '1',
+              song_number: 8,
+              show_date: '1997-12-29',
+            },
+            {
+              song: 'Run Like An Antelope',
+              set: '1',
+              song_number: 9,
+              show_date: '1997-12-29',
+            },
+            {
+              song: 'Down With Disease',
+              set: '2',
+              song_number: 1,
+              show_date: '1997-12-29',
+            },
+            {
+              song: 'David Bowie',
+              set: '2',
+              song_number: 2,
+              show_date: '1997-12-29',
+            },
+            {
+              song: 'Possum',
+              set: '2',
+              song_number: 3,
+              show_date: '1997-12-29',
+            },
+            {
+              song: 'You Enjoy Myself',
+              set: '2',
+              song_number: 4,
+              show_date: '1997-12-29',
+            },
+            {
+              song: 'Good Times Bad Times',
+              set: 'Encore',
+              song_number: 1,
+              show_date: '1997-12-29',
+            },
+          ]);
+        });
+      });
+    });
+
+    context('when a song performance is already in the database', () => {
+      beforeEach(() => {
+        const getSongId = () => {
+          const song = {
+            identifier: 'David Bowie',
+          };
+
+          return db('songs').insert(song).then(() => {
+            return db('songs').select().then((result) => {
+              return result[0].id;
+            });
+          });
+        };
+
+        const getSetId = () => {
+          const set = {
+            identifier: '2',
+          };
+
+          return db('sets').insert(set).then(() => {
+            return db('sets').select().then((result) => {
+              return result[0].id;
+            });
+          });
+        };
+
+        const getShowId = () => {
+          const getVenueId = () => {
+            const venue = {
+              identifier: 'Madison Square Garden',
+              country: 'us',
+              province: 'ny',
+            };
+
+            return db('venues').insert(venue).then((result) => {
+              return db('venues').select().then((result) => {
+                return result[0].id;
+              });
+            });
+          };
+
+          return getVenueId().then((venueId) => {
+            const show = {
+              date: '1997-12-29',
+              year: 1997,
+              month: 12,
+              day: 29,
+              venue_id: venueId,
+            };
+
+            return db('shows').insert(show).then(() => {
+              return db('shows').select().then((result) => {
+                return result[0].id;
+              });
+            });
+          });
+        };
+
+        return P.all([
+          getSongId(),
+          getSetId(),
+          getShowId(),
+        ]).spread((songId, setId, showId) => {
+          return db('song_performances').insert({
+            song_id: songId,
+            set_id: setId,
+            show_id: showId,
+            song_number_in_set: 2,
+          });
+        });
+      });
+
+      it('handles it gracefully', () => {
+        return show.sync('1997-12-29').then((result) => {
+          expect(result).to.deep.equal({
+            success: true,
+            reason: '',
+          });
+
+          return db('song_performances').select().then((rows) => {
             expect(rows.length).to.equal(14);
           });
         });
