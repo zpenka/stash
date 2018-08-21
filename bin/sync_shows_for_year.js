352ce80a067ce68de1bb52da9295567a1736c8dd
Zack Penka <zpenka@shutterstock.com>
2018-08-20 19:18:03 -0600

    Script to sync data for year

---
+++ bin/sync_shows_for_year.js
@@ -0,0 +1,49 @@
+const P = require('bluebird');
+const cli = require('cli');
+
+const log = require('../lib/log');
+const net = require('../lib/net');
+const show = require('../lib/show');
+
+const options = cli.parse({
+  year: ['y', 'Year to sync', 'int', false],
+  sleepTime: ['s', 'Seconds to sleep between syncs', 'int', 60],
+});
+
+if (!options.year) {
+  log.error('You must supply a year to sync');
+  cli.getUsage();
+
+  process.exit(1);
+}
+
+const sleep = options.sleepTime * 1000;
+if (!sleep) {
+  log.error('Sleep cannot be 0');
+  cli.getUsage();
+
+  process.exit(1);
+}
+
+const syncShowsForYear = (year) => {
+  return net.getShowsForYear(year).then((showDates) => {
+    return P.each(showDates, (showDate) => {
+      return show.sync(showDate)
+      .then(() => {
+        return P.delay(sleep);
+      });
+    });
+  })
+  .then(() => {
+    log.info(`All shows successfully synced for year ${year}`);
+
+    process.exit(0);
+  })
+  .catch((err) => {
+    log.error(err);
+
+    process.exit(1);
+  });
+};
+
+syncShowsForYear(options.year);
